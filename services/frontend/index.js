8476d3ec7e24d2455d004d792e46df7e30f68266
Lucas Pícoli <109911121+Picoleh@users.noreply.github.com>
2025-03-19 16:03:41 -0300

    Add files via upload

--- services/frontend/index.js
+++ services/frontend/index.js
@@ -52,6 +52,49 @@ function calculateShipping(id, cep) {
         });
 }
 
+function clickSearchByID(){
+    const books = document.querySelector('.books');
+    const valorID = document.getElementById("inputID").value;
+    //const junto = "http://localhost:3000/product/" + valorID;
+    //console.log("Valor:", junto);
+    fetch('http://localhost:3000/product/' + valorID)
+        .then((data) => {
+            if (data.ok) {
+                return data.json();
+            }
+            throw data.statusText;
+        })
+        .then((data) => {
+            console.log(data);
+            if (data) {
+                const divs = document.getElementsByClassName("column is-4");
+                console.log("tam:", divs.length);
+                for(let i = 0; i < divs.length; i++){
+                    divs[i].remove();
+                }
+                books.appendChild(newBook(data));
+
+                document.querySelectorAll('.button-shipping').forEach((btn) => {
+                    btn.addEventListener('click', (e) => {
+                        const id = e.target.getAttribute('data-id');
+                        const cep = document.querySelector(`.book[data-id="${id}"] input`).value;
+                        calculateShipping(id, cep);
+                    });
+                });
+
+                document.querySelectorAll('.button-buy').forEach((btn) => {
+                    btn.addEventListener('click', (e) => {
+                        swal('Compra de livro', 'Sua compra foi realizada com sucesso', 'success');
+                    });
+                });
+            }
+        })
+        .catch((err) => {
+            swal('Erro', 'Erro ao listar os produtos', 'error');
+            console.error(err);
+        });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const books = document.querySelector('.books');
 
